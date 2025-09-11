9eaa8afc43b2ad2ced1adc35debc619965be681c
Julian F. <julian.fiedler@edu.fh-joanneum.at>
2025-09-11 20:54:31 +0200

    Implemented FileSystemUtils

--- Pugna/src/main/java/at/minecraft/pugna/utils/FileSystemUtils.java
+++ Pugna/src/main/java/at/minecraft/pugna/utils/FileSystemUtils.java
@@ -1,5 +1,8 @@
 package at.minecraft.pugna.utils;
 
+import at.minecraft.pugna.config.ChatConfig;
+import org.bukkit.Bukkit;
+
 import java.io.File;
 
 public final class FileSystemUtils {
@@ -8,6 +11,35 @@ public final class FileSystemUtils {
     /* === Operations === */
 
     public static void deleteDirectory(File directory) {
-        // TODO: Implement method
+        if (directory == null || !directory.exists()) {
+            return;
+        }
+
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return;
+        }
+
+        for (File file : files) {
+            try {
+                if (file.isDirectory()) {
+                    deleteDirectory(file);
+                } else {
+                    if (!file.delete()) {
+                        file.deleteOnExit();
+                    }
+                }
+            } catch (Exception exception) {
+                Bukkit.getLogger().warning(ChatConfig.getRawPrefix() + "FileSystemUtils.deleteDirectory: Could not delete file: " + file.getAbsolutePath() + " (" + exception.getMessage() + ")");
+            }
+        }
+
+        try {
+            if (!directory.delete()) {
+                directory.deleteOnExit();
+            }
+        } catch (Exception exception) {
+            Bukkit.getLogger().warning(ChatConfig.getRawPrefix() + "FileSystemUtils.deleteDirectory: Could not delete directory: " + directory.getAbsolutePath() + " (" + exception.getMessage() + ")");
+        }
     }
 }
