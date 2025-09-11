8fd29db950c9764fcde8bda958e679e7d54295f3
Julian F. <julian.fiedler@edu.fh-joanneum.at>
2025-09-11 20:49:39 +0200

    Implemented ChatUtils

--- Pugna/src/main/java/at/minecraft/pugna/utils/ChatUtils.java
+++ Pugna/src/main/java/at/minecraft/pugna/utils/ChatUtils.java
@@ -1,5 +1,8 @@
 package at.minecraft.pugna.utils;
 
+import at.minecraft.pugna.teams.Team;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public final class ChatUtils {
@@ -8,15 +11,34 @@ public final class ChatUtils {
     /* === Operations === */
 
     public static void broadcast(String message) {
-        // TODO: Implement method
+        for (Player player : PlayerUtils.getAllOnlinePlayers()) {
+            player.sendMessage(message);
+        }
+
+        Bukkit.getLogger().info("[Global] " + ChatColor.stripColor(message));
     }
 
     public static void sendTeamMessage(Player sender, String message) {
-        // TODO: Implement method
+        if (sender == null || !sender.isOnline()) {
+            return;
+        }
+
+        Team team = TeamUtils.getTeam(sender);
+        if (team != null) {
+            team.broadcast(message);
+        }
+
+        Bukkit.getLogger().info("[Team] " + ChatColor.stripColor(message));
     }
 
     public static void sendSpectatorMessage(String message) {
-        // TODO: Implement method
+        for (Player player : PlayerUtils.getAllOnlinePlayers()) {
+            if (PlayerUtils.isSpectator(player)) {
+                player.sendMessage(message);
+            }
+        }
+
+        Bukkit.getLogger().info("[Spectator] " + message);
     }
 
     public static String formatMessage(Player sender, String message) {
