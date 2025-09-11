dd2e0f43c7f1dcd8a9086b5259f3320e079ac8e7
Julian F. <julian.fiedler@edu.fh-joanneum.at>
2025-09-12 00:20:30 +0200

    Implemented hub command

--- Pugna/src/main/java/at/minecraft/pugna/commands/HubCommand.java
+++ Pugna/src/main/java/at/minecraft/pugna/commands/HubCommand.java
@@ -1,13 +1,24 @@
 package at.minecraft.pugna.commands;
 
+import at.minecraft.pugna.chat.Message;
+import at.minecraft.pugna.config.ChatConfig;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HubCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments) {
-        // TODO: Implement method
+        if (!(sender instanceof Player)) {
+            String message = ChatConfig.getMessage(Message.PLAYER_ONLY_COMMAND);
+            sender.sendMessage(ChatColor.stripColor(message));
+            return true;
+        }
+
+        Player player = (Player) sender;
+        player.kickPlayer("");
         return true;
     }
 }
