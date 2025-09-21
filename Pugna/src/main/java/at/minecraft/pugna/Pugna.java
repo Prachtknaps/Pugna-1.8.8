2c01718e92ed4d90529768badf2d8f64c5ea0c8b
Julian F. <julian.fiedler@edu.fh-joanneum.at>
2025-09-21 10:43:18 +0200

    Set up project

---
+++ Pugna/src/main/java/at/minecraft/pugna/Pugna.java
@@ -0,0 +1,17 @@
+package at.minecraft.pugna;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pugna extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
